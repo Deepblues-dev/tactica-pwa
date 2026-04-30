e5c1e450fb374d8291b9cbe165559ca81ac34cfb
Deepblues-dev <mariofvg@gmail.com>
2026-04-30 15:18:20 -0600

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tactica-cache-v2';
+const CACHE_NAME = 'tactica-cache-v1.5';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
